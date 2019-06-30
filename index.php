ab13bbb698c7ae9564d9b7085713a7ba1d777fc6
ThomasS <thomas.stessels@gmail.com>
2019-06-30 13:27:14 +0200

    added basic website structure with sass

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="source/dist/css/app.css">
+</head>
+<body>
+    <nav>
+        <h1>TODO APP</h1>
+        <h2>Edit</h2>
+    </nav>
+    <span>Not Done</span>
+    <li><img src="images/checkF.png" alt=""><a href="#">Pay Rent</a><p>Due: Today @ 12:00</p></li>
+    <span>Done</span>
+    <li><img src="images/checkUF.png" alt=""><a href="#">Work on Class</a></li>
+</body>
+</html>
