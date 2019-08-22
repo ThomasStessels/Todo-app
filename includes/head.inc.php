1869a99716816cf0ed462928c4f7035ea38527be
ThomasS <thomas.stessels@gmail.com>
2019-08-22 06:22:09 +0200

    added includes for header

---
+++ includes/head.inc.php
@@ -0,0 +1,12 @@
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>ToDo App</title>
+    <meta name="description" content="My ToDo App">
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fontawesome/css/all.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
---
+++ includes/nav.inc.php
@@ -0,0 +1,31 @@
+<div class="head">
+    <div class="head-left">
+      <a href="create.php" class="btn">Create</a>
+    </div>
+    <div class="head-center">
+    <h1>TODO APP</h1>
+    </div>
+    <div class="head-right">
+      <div id="navToggle" class="slide-menu">
+        <div class="menu-open"><i class="fas fa-bars"></i></div>
+        <div class="menu-close"><i class="fas fa-times"></i></div>
+      </div>
+    </div>
+ </div> 
+   <div class="menu">
+    <ul>
+      <li><a href="./create.php">Create todo</a></li>
+      <li><a href="./createList.php"><li>Create list</a></li>
+      <li><a href="./lists.php"><li>lists</a></li>
+      <li><a href="./logout.php"><li class="menu__logout">Logout</a></li>
+    </ul>
+</div> 
+
+
+<script>
+$( document ).ready(function() {
+    $( "#navToggle" ).click(function() {
+      $(this).toggleClass('open');
+      $( ".menu" ).toggleClass( "active");
+    });
+});</script>
